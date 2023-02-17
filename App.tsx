092327f14e71ff78a0fcb664a361870c9a4f402d
benjamineruvieru <benjamineruvieru@gmail.com>
2023-02-17 21:04:48 +0100

    Remove unnecesary packages
    Former-commit-id: f83b98a45404d0f4017a634176c5cbfcc2ea95de

--- App.tsx
+++ App.tsx
@@ -1,27 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { View } from "react-native";
 import { ApolloProvider } from "@apollo/client";
-import { client, persistor } from "./source/graphql/client";
-import { Provider } from "react-redux";
-import { store } from "./source/redux/store";
+import { client } from "./source/graphql/client";
 import AppNavigator from "./source/navigation";
 
 function App(): JSX.Element {
-  useEffect(() => {
-    async function initializeCache() {
-      await persistor.restore();
-    }
-    initializeCache();
-  }, []);
-
   return (
-    <Provider store={store}>
-      <ApolloProvider client={client}>
-        <View>
-          <AppNavigator />
-        </View>
-      </ApolloProvider>
-    </Provider>
+    <ApolloProvider client={client}>
+      <View>
+        <AppNavigator />
+      </View>
+    </ApolloProvider>
   );
 }
 
